ff600a1faec0632e79ffccc24482fce681a88225
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-07-09 09:31:48 +0300

    complete task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,13 +3,53 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-/* <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
+const gallery = document.querySelector(".gallery");
+const body = document.querySelector("body");
+
+const imgArr = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</li> */
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", imgArr);
+
+gallery.addEventListener("click", onPhotoClick);
+
+function onPhotoClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName === "IMG") {
+    const largePhotoLink = evt.target.dataset.source;
+    const instance = basicLightbox.create(
+      `
+		<img width="1400" height="900" src="${largePhotoLink}">
+	`,
+      {
+        onShow: () => {
+          document.addEventListener("keydown", closeModal);
+          body.classList.add("no-scroll");
+        },
+        onClose: () => {
+          document.removeEventListener("keydown", closeModal);
+          body.classList.remove("no-scroll");
+        },
+      }
+    );
+    instance.show();
+
+    function closeModal(evt) {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    }
+  }
+}
